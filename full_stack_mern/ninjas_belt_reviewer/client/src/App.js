48122ce7b0223e2bcd20d3d5ba15b2e4b58daaba
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2021-11-09 14:45:24 -0800

    details

--- full_stack_mern/ninjas_belt_reviewer/client/src/App.js
+++ full_stack_mern/ninjas_belt_reviewer/client/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 import AllNinjas from './components/AllNinjas';
 import NewNinjaForm from './components/NewNinjaForm';
+import OneNinjaDetails from './components/OneNinjaDetails';
 import {
   BrowserRouter,
   Switch,
@@ -15,6 +16,7 @@ function App() {
     <BrowserRouter>
       <div className="App container">
         <h1>Hello Ninjas Belt Reviewer</h1>
+        <Link className= "btn btn-primary" to= "/">Home</Link>
         <Switch>
           <Route exact path= "/">
             <NewNinjaForm formSubmitted = {formSubmitted} setFormSubmitted= {setFormSubmitted}/>
@@ -23,6 +25,13 @@ function App() {
             <AllNinjas formSubmitted = {formSubmitted}></AllNinjas>
 
           </Route>
+
+          <Route exact path= "/ninjas/:id">
+            <OneNinjaDetails></OneNinjaDetails>
+          </Route>
+
+          
+
 
         </Switch>
       </div>
